d34535df8e3b711bc122a70f5cef248306b665f2
Nikhil Kaushik <nikhil.celtics@gmail.com>
2018-11-06 14:56:09 +0530

    adds assignment 8

---
+++ 8/client.c
@@ -0,0 +1,108 @@
+#include <stdio.h> /* printf, sprintf */
+#include <stdlib.h> /* exit */
+#include <unistd.h> /* read, write, close */
+#include <string.h> /* memcpy, memset */
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
+
+void error(const char *msg) { perror(msg); exit(0); }
+
+int main(int argc,char *argv[])
+{
+    /* first what are we going to send and where are we going to send it? */
+    int portno =        80;
+    char *host =        "localhost";
+    char *message_fmt = "GET / HTTP/1.0\r\n\r\n Host:%s";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, received, total;
+    char message[1024],response[50000];
+
+    /* fill in the parameters */
+    sprintf(message,message_fmt, host);
+    printf("Request:\n%s\n",message);
+
+    if (argc > 1) {
+        host = argv[1];
+    }
+
+    /* create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) error("ERROR opening socket");
+
+    /* lookup the ip address */
+    server = gethostbyname(host);
+    if (server == NULL) error("ERROR, no such host");
+
+    /* fill in the structure */
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        error("ERROR connecting");
+
+    /* send the request */
+    total = strlen(message);
+    sent = 0;
+    do {
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            error("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent+=bytes;
+    } while (sent < total);
+
+    /* receive the response */
+    memset(response,0,sizeof(response));
+    total = sizeof(response)-1;
+    received = 0;
+    do {
+        bytes = read(sockfd,response+received,total-received);
+        if (bytes < 0)
+            error("ERROR reading response from socket");
+        if (bytes == 0)
+            break;
+        received+=bytes;
+    } while (received < total);
+
+    printf("Response:\n%s\n",response);
+
+    if (received == total)
+//        error("ERROR storing complete response from socket");
+
+    /* close the socket */
+    close(sockfd);
+
+    char htmlBody[50000];
+    int start = 0;
+    for (int i=0;i<received;i++) {
+        if (response[i] == '<') {
+            break;
+        }
+        start++;
+    }
+
+    int j = 0;
+    for (int i=start;i<received;i++) {
+        htmlBody[j] = response[i];
+        j++;
+    }
+    htmlBody[j] = '\0';
+
+    /* process response */
+    printf("Response:\n%s\n",response);
+
+    FILE *f = fopen("/tmp/cache.html", "wb");
+    fwrite(htmlBody, sizeof(char), 50000, f);
+    fclose(f);
+
+    system("firefox /tmp/cache.html");
+
+    return 0;
+}
